2564621bdeab68b41ac7007185b1059563c09f55
edsondearaujo <edsondearaujofga@gmail.com>
2024-10-03 14:00:08 -0300

    #8 functional main v1

--- src/main.py
+++ src/main.py
@@ -1,25 +1,13 @@
 from services.relatorio import Relatorio
-from database.db_manager import DatabaseManager
-from utils.excel_helper import read_excel, write_excel
+from services.carregar_vendas import carregar_vendas
 
 def main():
-    # Caminho para o arquivo de vendas
     vendas_file_path = 'data/vendas.xlsx'
-    # Caminho para o arquivo de banco de dados (CSV)
-    db_file_path = 'data/outros_dados.csv'
+    vendas = carregar_vendas(vendas_file_path)
 
-    # Leitura dos dados de vendas
-    vendas_df = read_excel(vendas_file_path)
+    relatorio = Relatorio(vendas)
 
-    # Instanciação do gerenciador de banco de dados
-    db_manager = DatabaseManager(db_file_path)
-
-    # Supondo que você tenha uma lógica para processar as vendas
-    relatorio = Relatorio(vendas_df)
-    relatorio.gerar_relatorio()
-
-    # Aqui você pode adicionar qualquer outra lógica necessária, como salvar os dados no banco
-    db_manager.write_data(vendas_df)
+    print(relatorio)
 
 if __name__ == "__main__":
     main()
