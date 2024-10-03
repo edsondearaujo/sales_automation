fc8ea616648b10b7741300b3eff4833dc6d93425
edsondearaujo <edsondearaujofga@gmail.com>
2024-10-03 14:02:22 -0300

    #2 functional venda v1

--- src/models/venda.py
+++ src/models/venda.py
@@ -1,8 +1,9 @@
 # models/venda.py
 
 class Venda:
-    def __init__(self, produto, quantidade, preco_unitario, data_venda):
-        self.produto = produto          # Objeto Produto
+    def __init__(self, id, produto, quantidade, preco_unitario, data_venda):
+        self.id = id
+        self.produto = produto          # Deve ser um objeto da classe Produto
         self.quantidade = quantidade
         self.preco_unitario = preco_unitario
         self.data_venda = data_venda
@@ -13,7 +14,7 @@ class Venda:
 
     def __str__(self):
         """Representação em string da venda."""
-        return (f"Venda: {self.produto.nome}, "
+        return (f"Venda: {self.produto.nome}, " 
                 f"Quantidade: {self.quantidade}, "
                 f"Preço Unitário: R$ {self.preco_unitario:.2f}, "
                 f"Data: {self.data_venda}, "
