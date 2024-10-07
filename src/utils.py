947f9e935b0d5efbb17bea193a11905b5ba5b99a
edsondearaujo <edsondearaujofga@gmail.com>
2024-10-07 03:55:27 -0300

    #23 google dependencies added

--- src/utils.py
+++ src/utils.py
@@ -1,33 +1,35 @@
-import os
-import gspread
-from google.oauth2.service_account import Credentials
+#src/utils.py
+import logging
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+from src.auth import autenticar_google_sheets
 
-# Defina os escopos necessários
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
 
-def autenticar_google_sheets():
-    """Autentica na API do Google Sheets usando as credenciais fornecidas."""
-    credenciais_path = './data/credentials.json'
-
-    try:
-        creds = Credentials.from_service_account_file(credenciais_path, scopes=SCOPES)
-        return build("sheets", "v4", credentials=creds)
-    except Exception as e:
+def carregar_dados_aba(spreadsheet_id, range_name):
+    """Carrega os dados de uma aba da planilha Google Sheets."""
+    print(f"Iniciando o carregamento de dados da aba: {range_name}")
+    creds = autenticar_google_sheets()
+    if not creds:
+        logging.error("Erro: Falha na autenticação.")
+        print("Erro na autenticação")
         return None
 
-
-def carregar_dados_planilha(nome_planilha, aba):
-    """Carrega os dados de uma planilha específica do Google Sheets."""
     try:
-        service = autenticar_google_sheets()
-
-        # Faz a requisição para ler os dados da planilha
+        print(f"Autenticação bem-sucedida. Tentando acessar a aba {range_name}.")
+        service = build('sheets', 'v4', credentials=creds)
         sheet = service.spreadsheets()
-        result = sheet.values().get(spreadsheetId=nome_planilha, range=aba).execute()
-        values = result.get('values', [])
 
+        print(f"Acessando a planilha com ID: {spreadsheet_id}, Range: {range_name}")
+        result = sheet.values().get(spreadsheetId=spreadsheet_id, range=range_name).execute()
+
+        values = result.get('values', [])
+        if not values:
+            logging.info(f"Nenhum dado encontrado na aba {range_name}.")
+            print(f"Nenhum dado encontrado na aba {range_name}.")
+            return []
+
+        print(f"Dados carregados da aba {range_name}:", values)
         return values
-    except HttpError as err:
+    except Exception as e:
+        logging.error(f"Erro ao acessar a API do Google Sheets: {e}")
+        print(f"Erro ao acessar a API do Google Sheets: {e}")
         return None
